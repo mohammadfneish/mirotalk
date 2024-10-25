3fb4f38b5af8ca734f124fbba71130ea8cb64010
BasselBazzoun <63313425+BasselBazzoun@users.noreply.github.com>
2024-10-25 14:16:54 +0300

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -10629,7 +10629,7 @@ function leaveFeedback() {
         showDenyButton: true,
         background: swBg,
         imageUrl: images.feedback,
-        title: 'Leave a feedback',
+        title: 'Leave a feedback11',
         text: 'Do you want to rate your ilearncenters experience?',
         confirmButtonText: `Yes`,
         denyButtonText: `No`,
